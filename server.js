a8e648187937e0fd001da6726d2c7abb591d7bcc
Murillo Silva Vieira <48219501+M-Silva-V@users.noreply.github.com>
2021-11-18 12:31:14 -0300

    Alteracao

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/teste-bten"));
+app.use(express.static(__dirname + "/teste-bten/dist"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/teste-bten/index.html"));
+  res.sendFile(path.join(__dirname + "/teste-bten/dist/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
